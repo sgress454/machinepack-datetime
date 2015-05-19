80fda433336785618020a39f11638d564889d887
Mike McNeil <mike@balderdash.co>
2015-05-19 12:31:35 -0500

    Finished time-from and added tests.

--- machines/time-from.js
+++ machines/time-from.js
@@ -15,11 +15,10 @@ module.exports = {
 
   inputs: {
 
-    timestamp: {
-      friendlyName: 'Timestamp',
+    toWhen: {
+      friendlyName: 'To...',
       description: 'An epoch offset (in milliseconds)',
       moreInfoUrl: 'http://momentjs.com/docs/#/parsing/unix-offset/',
-      extendedDescription: 'The number of milliseconds since midnight (GMT/UTC) on January 1, 1970.',
       example: 1318781876000,
       required: true
     },
@@ -27,9 +26,9 @@ module.exports = {
     fromWhen: {
       friendlyName: 'From...',
       description: 'Another epoch offset (in milliseconds) to use as a reference when formatting the "time from" string.',
+      extendedDescription: 'Defaults to current date/time',
       moreInfoUrl: 'http://momentjs.com/docs/#/parsing/unix-offset/',
-      example: 1318781870000,
-      required: true
+      example: 1318781870000
     },
 
   },
@@ -37,8 +36,18 @@ module.exports = {
 
   exits: {
 
+    invalidToWhen: {
+      friendlyName: 'invalid "to"',
+      description: 'Could not build a date/time/zone from the provided "To..." timestamp.',
+    },
+
+    invalidFromWhen: {
+      friendlyName: 'invalid "from"',
+      description: 'Could not build a date/time/zone from the provided "From..." timestamp.',
+    },
+
     success: {
-      variableName: 'timeAgo',
+      variableName: 'timeFromStr',
       example: '6 seconds ago',
       description: 'Done.'
     },
@@ -47,7 +56,25 @@ module.exports = {
 
 
   fn: function (inputs,exits) {
-    return exits.success();
+    var MomentTz = require('moment-timezone');
+
+    // Build moment obj for `toWhen`
+    var toWhenObj = MomentTz.tz(new Date(inputs.toWhen), 'Etc/Greenwich');
+    if (!toWhenObj.isValid()) {
+      return exits.invalidToWhen();
+    }
+
+    // Build moment obj for `fromWhen`
+    // (default to current date/time if no `fromWhen` timestamp was provided)
+    inputs.fromWhen = (typeof inputs.fromWhen === 'undefined') ? (new Date()).getTime() : inputs.fromWhen;
+    var fromWhenObj = MomentTz.tz(new Date(inputs.fromWhen), 'Etc/Greenwich');
+    if (!fromWhenObj.isValid()) {
+      return exits.invalidFromWhen();
+    }
+
+    // Format final "time from" string
+    var resultStr = toWhenObj.from(fromWhenObj);
+    return exits.success(resultStr);
   },
 
 
