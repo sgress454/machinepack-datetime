336da2b78e99a496b6c94d508d4ab1c155b73a3f
Rachael Shaw <r@rachael.wtf>
2015-06-10 12:32:16 -0500

    Added a 'Now' machine

---
+++ machines/now.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+
+  friendlyName: 'Now',
+
+
+  description: 'Construct a new timestamp from the current time.',
+
+
+  sync: true,
+
+
+  inputs: {
+
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'Returns the current time as a JS timestamp (an epoch offset in milliseconds)',
+      moreInfoUrl: 'http://momentjs.com/docs/#/parsing/unix-offset/',
+      extendedDescription: 'The number of milliseconds since midnight (GMT/UTC) on January 1, 1970.',
+      example: 1318781876000,
+    }
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    return exits.success((new Date()).getTime());
+  }
+
+
+};
